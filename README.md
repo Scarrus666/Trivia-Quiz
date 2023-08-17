df2eff57a4a774061220e46fa9a8be612f11eb7a
Scarrus666 <scarrus@gmail.com>
2023-08-17 12:30:25 +0200

    finished error messages

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Trivia-Quiz
 Opportunity Project Nr. 7
+
+
+This is a Quiz about Geography, Biology, Zoology, Languages and more
+
+This Web Application consists of three (3) main parts:
+
+    - Index.php which serves as landing page, home page and introduction to the Quiz
+    - question.php serves as the template page for questions and uses some other adjacent pages to loop through the questions
+    - report.php is the final page and is used to display a summary of the questions and answers as well as an evaluation
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Take a Quiz!</title>
+    <link rel="stylesheet" href="./css/style.css"/>
+    <link rel="stylesheet" href="./css/mystyle.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+
+<body>
+
+    <?php
+        require_once './tools/tools.php';
+        
+        err_rnd();
+    ?>
+
+</body>
+</html>
